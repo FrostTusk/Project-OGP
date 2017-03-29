d6b21144f0c589fef42f371975ec66aae4004993
Mathijs Hubrechtsen <Mathijs Hubrechtsen>
2017-03-29 21:38:51 +0200

    Fixed some annotations.

--- OGP1617-Asteroids/src/asteroids/model/Bullet.java
+++ OGP1617-Asteroids/src/asteroids/model/Bullet.java
@@ -1,5 +1,141 @@
 package asteroids.model;
 
+import be.kuleuven.cs.som.annotate.Basic;
+import be.kuleuven.cs.som.annotate.Immutable;
+import be.kuleuven.cs.som.annotate.Raw;
+
 public class Bullet {
 
+	/**
+	 * Variable registering the X position of this bullet.
+	 */
+	private double positionX;
+	/**
+	 * Variable registering the minimum X position of this bullet.
+	 */
+	private final double minPositionX = Double.NEGATIVE_INFINITY;
+	/**
+	 * Variable registering the maximum X position of this bullet.
+	 */
+	private final double maxPositionX = Double.POSITIVE_INFINITY;
+	/**
+	 * Variable registering the Y position of this bullet.
+	 */
+	private double positionY;
+	/**
+	 * Variable registering the minimum Y position of this bullet.
+	 */
+	private final double minPositionY = Double.NEGATIVE_INFINITY;
+	/**
+	 * Variable registering the maximum Y position of this bullet.
+	 */
+	private final double maxPositionY = Double.POSITIVE_INFINITY;
+	
+	
+	/**
+	 * Return the X position of this bullet.
+	 */
+	@Basic @Raw
+	public double getPositionX() {
+		return this.positionX;
+	}
+	
+	/**
+	 * Return the minimum X position of this bullet.
+	 */
+	@Basic @Immutable @Raw
+	public double getMinPositionX() {
+		return this.minPositionX;
+	}
+	
+	/**
+	 * Return the max X position of this bullet.
+	 */
+	@Basic @Immutable @Raw 
+	public double getMaxPositionX() {
+		return this.maxPositionX;
+	}
+	
+	/**
+	 * Return the Y position of this bullet.
+	 */
+	@Basic @Raw
+	public double getPositionY() {
+		return this.positionY;
+	}
+	
+	/**
+	 * Return the minimum Y position of this bullet.
+	 */
+	@Basic @Immutable @Raw
+	public double getMinPositionY() {
+		return this.minPositionY;
+	}
+	
+	/**
+	 * Return the max Y position of this bullet.
+	 */
+	@Basic @Immutable @Raw 
+	public double getMaxPositionY() {
+		return this.maxPositionY;
+	}
+	
+	
+	/**
+	 * Check whether the given position is a valid position for
+	 * any bullet.
+	 *  
+	 * @param  	positionX
+	 *         	The X position to check.
+	 * @param	positionY
+	 * 			The Y position to check.
+	 * @return	Returns whether or not the given position is a valid position.
+	 * 			true if it is, false if not.
+	 *       	| result == (! ( (java.lang.Double.isNaN(positionX)) || (java.lang.Double.isNaN(positionY)) ) &&
+	 *       	|			(this.getMinPositionX() < positionX) && (this.getMaxPositionX() > positionX) &&
+	 *			|			(this.getMinPositionY() < positionY) && (this.getMaxPositionY() > positionY)      
+	 */
+	public boolean isValidPosition(double positionX, double positionY) {
+		// A position has to be a number.
+		if ( (java.lang.Double.isNaN(positionX)) || (java.lang.Double.isNaN(positionY)) )
+			return false;
+		// A position cannot be smaller/bigger or equal to negative/positive infinity.
+		if ( (getMinPositionX() < positionX) && (getMaxPositionX() > positionX) &&
+				(getMinPositionY() < positionY) && (getMaxPositionY() > positionY) )
+			return true;
+		else
+			return false;	
+	}
+	
+	
+	/**
+	 * Set the position of this bullet to the given position.
+	 * 
+	 * @param  	positionX
+	 *         	The new X position for this bullet.
+	 * @param  	positionY
+	 * 		   	The new Y position for this bullet.
+	 * 
+	 * @post   	The X position of this new bullet is equal to
+	 *         	the given X position.
+	 *       	| new.getPositionX() == positionX
+	 * @post   	The Y position of this new bullet is equal to
+	 *         	the given Y position.
+	 *       	| new.getPositionY() == positionY
+	 *       
+	 * @throws 	IndexOutOfBoundsException
+	 *         	The given position is not a valid position for any
+	 *         	bullet.
+	 *       	| ! isValidPosition(getPositionX(), getPositionY())
+	 */
+	@Raw
+	public void setPosition(double positionX, double positionY) 
+			throws IndexOutOfBoundsException {
+		if (! isValidPosition(positionX, positionY))
+			throw new IndexOutOfBoundsException();
+		this.positionX = positionX;
+		this.positionY = positionY;
+	}
+	
+	
 }
--- OGP1617-Asteroids/src/asteroids/model/Ship.java
+++ OGP1617-Asteroids/src/asteroids/model/Ship.java
@@ -144,7 +144,7 @@ public class Ship {
 	/**
 	 * Return the X position of this ship.
 	 */
-	@Basic @Raw
+	@Basic 
 	public double getPositionX() {
 		return this.positionX;
 	}
@@ -237,7 +237,6 @@ public class Ship {
 	 *         	ship.
 	 *       	| ! isValidPosition(getPositionX(), getPositionY())
 	 */
-	@Raw
 	public void setPosition(double positionX, double positionY) 
 			throws IndexOutOfBoundsException {
 		if (! isValidPosition(positionX, positionY))
@@ -273,7 +272,7 @@ public class Ship {
 	/**
 	 * Return the velocity of this ship.
 	 */
-	@Basic @Raw
+	@Basic 
 	public double getVelocityX() {
 		return this.velocityX;
 	}
@@ -281,7 +280,7 @@ public class Ship {
 	/**
 	 * Return the velocity of this ship.
 	 */
-	@Basic @Raw
+	@Basic 
 	public double getVelocityY() {
 		return this.velocityY;
 	}
@@ -373,7 +372,7 @@ public class Ship {
 	/**
 	 * Return the orientation of this ship.
 	 */
-	@Basic @Raw
+	@Basic 
 	public double getOrientation() {
 		return this.orientation;
 	}
@@ -409,7 +408,6 @@ public class Ship {
 	 *         	orientation.
 	 *       	| new.getOrientation() == orientation
 	 */
-	@Raw
 	public void setOrientation(double orientation) {
 		assert isValidOrientation(orientation);
 		this.orientation = orientation;
@@ -446,7 +444,7 @@ public class Ship {
 	/**
 	 * Return the radius of this ship.
 	 */
-	@Basic @Immutable @Raw
+	@Basic  
 	public double getRadius() {
 		return this.radius;
 	}
@@ -461,7 +459,6 @@ public class Ship {
 	 * 			as a valid radius or not. true if it can, false if not.
 	 *       	| result == (POSITIVE_INFINITY > radius) && (radius >= this.getMinRadius())
 	*/
-	@Raw
 	public boolean canHaveAsRadius(double radius) {
 		if ( (Double.POSITIVE_INFINITY > radius) && (radius >= this.getMinRadius()) )
 			return true;
