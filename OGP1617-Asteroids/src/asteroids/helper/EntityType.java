1f0bb3d7d654716ae55f72d276e402e111cb5a19
Mathijs Hubrechtsen <mathijs.hubrechtsen@gmail.com>
2017-04-30 22:26:34 +0200

    Added opgaves/ifacade-3.

---
+++ OGP1617-Asteroids/src/asteroids/helper/EntityType.java
@@ -0,0 +1,9 @@
+package asteroids.helper;
+
+public enum EntityType {
+    SHIP,
+    BULLET,
+    MINORPLANET,
+    ASTEROID,
+    PLANETOID,
+}
---
+++ OGP1617-Asteroids/src/asteroids/helper/MinorPlanet.java
@@ -0,0 +1,53 @@
+package asteroids.helper;
+
+import asteroids.model.Ship;
+
+public class MinorPlanet extends Entity {
+
+	public MinorPlanet() {
+		this.minRadius = 5;
+	}
+	
+	@Override
+	public void terminate() {
+		if (isTerminated()) return;
+		if (getWorld() != null) { 
+			try {
+				getWorld().removeEntity(this);
+			}
+			catch (IllegalArgumentException exc) {}	// Empty catch because if an IllegalArgument Exception
+		}								// is thrown, it means that the association wasn't set to begin with.
+		// This means that the association already doesn't exist. We don't have to do anything.
+		this.isTerminated = true;
+	}
+
+
+	@Override
+	public void resolveCollisionShip(Ship ship) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void resolveCollisionMinorPlanet(MinorPlanet minorPlanet) {
+		if (minorPlanet == null) throw new NullPointerException();	// This method is pretty much a copy of the formula in the assignment.
+		double[] deltaV = {minorPlanet.getVelocityX() - getVelocityX(), minorPlanet.getVelocityY() - getVelocityY()};
+		double[] deltaR = {minorPlanet.getPosition().getPositionX() - getPosition().getPositionX(), 
+						   minorPlanet.getPosition().getPositionY() - getPosition().getPositionY()};
+		
+		double J = ( 2 * getMass() * minorPlanet.getMass() * helper.evaluateScalar(deltaV, deltaR) 
+				/ ( (getRadius() + minorPlanet.getRadius()) * (getMass() + minorPlanet.getMass()) ) );
+		
+		double Jx = J * (minorPlanet.getPosition().getPositionX() - getPosition().getPositionX()) / (getRadius() + minorPlanet.getRadius());
+		double Jy = J * (minorPlanet.getPosition().getPositionY() - getPosition().getPositionY()) / (getRadius() + minorPlanet.getRadius());
+		
+		this.setVelocity(getVelocityX() + Jx/getMass(), getVelocityY() + Jy/getMass());
+		minorPlanet.setVelocity(minorPlanet.getVelocityX() - Jx/minorPlanet.getMass(), minorPlanet.getVelocityY() - Jy/minorPlanet.getMass());
+	}
+
+	@Override
+	public EntityType getType() {
+		return EntityType.MINORPLANET;
+	}
+
+}
